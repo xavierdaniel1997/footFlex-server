d0285be4844f79a90c146425ff757be5b1ebb07e
Daniel Xavier <danielcx@gmail.com>
2024-09-27 18:24:58 +0530

    change the cors url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ connectDB()
 
 const allowedOrigins = process.env.CLIENT_ORIGINS
   ? process.env.CLIENT_ORIGINS.split(',')
-  : ['http://localhost:3000'];
+  : ['https://footflex-client.vercel.app/'];
 
 console.log("Allowed Origins: ", allowedOrigins);
 
