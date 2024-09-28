dc3a9212b65f920e35ea7e8683689b09b64a9c15
Daniel Xavier <danielcx@gmail.com>
2024-09-28 09:59:31 +0530

    change the generateToken secure to true

--- utils/genetateToken.js
+++ utils/genetateToken.js
@@ -5,7 +5,8 @@ export const generateToken = (res, user) => {
 
     res.cookie("jwtToken", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        // secure: process.env.NODE_ENV === "production",
+        secure: true,
         maxAge: 60 * 60 * 1000,
         sameSite: 'None'
     })
